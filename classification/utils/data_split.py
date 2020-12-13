fd50977323c1d1e9bdb4c3e2c97404a9f43ca0eb
edwin arkel 107 <edwin-2310@hotmail.com>
2020-12-13 16:57:30 +0800

    added files for making data_dic and splitting into train and test sets, along with the train and test set dictionaries

--- classification/utils/data_split.py
+++ classification/utils/data_split.py
@@ -2,7 +2,7 @@ import os, sys, glob
 import pandas as pd
 import numpy as np
 
-def data_split(data_folder, split):
+def data_split(data_dic_path, split):
     # splits data into training and testing
     '''
     for each class count the number of instances
@@ -40,14 +40,53 @@ def data_split(data_folder, split):
     has already reached its limit, then break;
     that way it will keep breaking on that class until it reaches the next class
     '''
+    
+    df = pd.read_csv(data_dic_path, sep=',', names=['class_id', 'dir'])
+    print('Original df: ', len(df))
+    
+    samples_per_class_df = df.groupby('class_id', as_index=True).count()
+    
+    df_list_train = []
+    df_list_test = []
+    for class_id, total_samples_class in enumerate(samples_per_class_df['dir']):
+        train_samples_class = int(total_samples_class*split[0])
+        test_samples_class = total_samples_class - train_samples_class
+        assert(train_samples_class+test_samples_class==total_samples_class)
+        train_subset_class = df.loc[df['class_id']==class_id].groupby('class_id').head(train_samples_class)
+        test_subset_class = df.loc[df['class_id']==class_id].groupby('class_id').tail(test_samples_class)
+        df_list_train.append(train_subset_class)
+        df_list_test.append(test_subset_class)
+    
+    df_train = pd.concat(df_list_train)
+    df_test = pd.concat(df_list_test)
+
+    print('Train df: ')
+    print(df_train.head())
+    print(df_train.shape)
+    print('Test df: ')
+    print(df_test.head())
+    print(df_test.shape)
+
+    dataset_name = os.path.basename(os.path.normpath(data_dic_path))
+    df_train_name = dataset_name + '_train.csv'
+    df_train.to_csv(df_train_name, sep=',', header=False, index=False)
+
+    df_test_name = dataset_name + '_test.csv'
+    df_test.to_csv(df_test_name, sep=',', header=False, index=False)
+    print('Finished saving train and test split dictionaries.')
+    
 
 def main():
-    data_folder = os.path.abspath(sys.argv[1])
-    split_train = sys.argv[2]
-    split_val = sys.argv[3]
-    split_test = sys.argv[4]
-    if (split_train+split_val+split_test == 1):
-        split = [split_train, split_val, split_test]
-    else:
-        split = [0.7, 0.1, 0.2]
-    data_split(data_folder, split)
+    data_dic_path = os.path.abspath(sys.argv[1])
+    try:
+        split_train = float(sys.argv[2])   # % of data for train (def: 0.8)
+        split_test = float(sys.argv[3])     # % of data for val (def: 0.2)
+        assert split_train+split_test==1, 'Arguments for split ratios should add up to 1'
+        split = [split_train, split_test]
+    except:
+        split = [0.8, 0.2]
+    
+    print(split)
+    data_split(data_dic_path, split)
+
+main()

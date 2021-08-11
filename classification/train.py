fixed bugs, added shuffling, option for masking behavior, and tag-level tokenizing

classification/train.py
@@ -12,6 +12,7 @@ import wandb
 from transformers import BertTokenizer
 
 import utilities as utilities
+from utilities.build_vocab import Vocabulary
 
 logger = logging.getLogger(__name__)
 
@@ -36,6 +37,30 @@ def environment_loader(args, init=True):
     args.num_classes = train_set.num_classes
     classid_classname_dic = train_set.classes
 
+    steps_per_epoch = len(train_loader)
+    total_steps = args.no_epochs * steps_per_epoch
+
+    # mask scheduler
+    if args.mask_schedule:
+        if args.tokenizer == 'wp':
+            tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+            args.vocab_size = tokenizer.vocab_size
+        elif args.tokenizer == 'tag':
+            tokenizer = utilities.custom_tokenizer.CustomTokenizer(
+                vocab_path=os.path.join(args.dataset_path, 'labels', 'vocab.pkl'), 
+                max_text_seq_len=args.max_text_seq_len)
+            args.vocab_size = tokenizer.vocab_size
+
+        mask_wucd_steps = int(total_steps * args.mask_wucd_percent)
+        mask_scheduler = utilities.scheduler.MasksSchedule(device=device, mask_schedule=args.mask_schedule, 
+            masking_behavior=args.masking_behavior, vocab_size=args.vocab_size,
+            batch_size=args.batch_size, max_text_seq_len=args.max_text_seq_len, 
+            warmup_steps=mask_wucd_steps, cooldown_steps=mask_wucd_steps, total_steps=total_steps, cycles=.5)
+    else:
+        mask_scheduler = None
+        tokenizer = None
+        args.vocab_size = None
+
     # model
     model = utilities.model_selection.load_model(args, device)
     if (args.model_name not in ['shallow', 'efficientnetb0', 'resnet18', 'resnet50', 'resnet152']) and init:
@@ -50,24 +75,11 @@ def environment_loader(args, init=True):
             params_to_update.append(param)
     optimizer = torch.optim.SGD(params_to_update, lr=args.learning_rate, momentum=0.9)
 
-    steps_per_epoch = len(train_loader)
-    total_steps = args.no_epochs * steps_per_epoch
     if args.lr_scheduler == 'warmupCosine':
         lr_scheduler = utilities.scheduler.WarmupCosineSchedule(optimizer, 
         warmup_steps=args.warmup_steps, t_total=total_steps)
     else:
         lr_scheduler=None
-    
-    # mask scheduler
-    if args.mask_schedule:
-        mask_wucd_steps = int(total_steps * args.mask_wucd_percent)
-        mask_scheduler = utilities.scheduler.MasksSchedule(device=device, mask_schedule=args.mask_schedule, 
-            batch_size=args.batch_size, max_text_seq_len=args.max_text_seq_len, 
-            warmup_steps=mask_wucd_steps, cooldown_steps=mask_wucd_steps, total_steps=total_steps, cycles=.5)
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    else:
-        mask_scheduler = None
-        tokenizer = None
     
     if init:
         return [f, device, train_set, train_loader, val_loader, test_loader, 

classification/utilities/custom_tokenizer.py
@@ -1,15 +1,14 @@
-import ast
+import torch
 import pickle
-import argparse
-import pandas as pd
-from collections import Counter
-
+from .build_vocab import Vocabulary
 
 class CustomTokenizer(object):
-    def __init__(self, vocab, max_text_seq_len, shuffle):
+    def __init__(self, vocab_path, max_text_seq_len, ret_tensor=True):
+        with open(vocab_path, 'rb') as f:
+            self.vocab = pickle.load(f)
+            self.vocab_size = len(self.vocab)
         self.max_text_seq_len = max_text_seq_len
-        self.vocab = vocab
-        self.shuffle = shuffle
+        self.ret_tensor = ret_tensor
 
     def __call__(self, tag_list):
         no_tokens = len(tag_list) + 2
@@ -19,7 +18,7 @@ class CustomTokenizer(object):
         tokens.append(self.vocab('[CLS]'))
 
         if no_tokens > self.max_text_seq_len:
-            tokens.extend([self.vocab(tag) for tag in tag_list[:self.max_text_seq_len-2])
+            tokens.extend([self.vocab(tag) for tag in tag_list[:self.max_text_seq_len-2]])
             tokens.append(self.vocab('[SEP]'))
         elif no_tokens < self.max_text_seq_len:
             tokens.extend([self.vocab(tag) for tag in tag_list])
@@ -29,72 +28,15 @@ class CustomTokenizer(object):
             tokens.extend([self.vocab(tag) for tag in tag_list])
             tokens.append(self.vocab('[SEP]'))
 
-        return torch.tensor(tokens, dtype=torch.int64)
-
-
-class Vocabulary(object):
-    """Simple vocabulary wrapper."""
-    def __init__(self):
-        self.word2idx = {}
-        self.idx2word = {}
-        self.idx = 0
-
-    def add_word(self, word):
-        if not word in self.word2idx:
-            self.word2idx[word] = self.idx
-            self.idx2word[self.idx] = word
-            self.idx += 1
-
-    def __call__(self, word):
-        if not word in self.word2idx:
-            return self.word2idx['[UNK]']
-        return self.word2idx[word]
-
-    def __len__(self):
-        return len(self.word2idx)
-
-
-def build_vocab(args):
-    """Build a simple vocabulary wrapper."""
-    df = pd.read_csv(args.caption_path, usecols=['tags_cat0'])
-    counter = Counter()
-    for i, tag in enumerate(df.tags_cat0):
-        tokens = ast.literal_eval(tag)
-        counter.update(tokens)
-
-    # If the word frequency is less than 'threshold', then the word is discarded.
-    print('Number of unique tokens: ', len(counter))
-    print('Total number of tokens: ', sum(counter.values()))
-    words = [word for word, cnt in counter.items() if cnt >= args.threshold]
-
-    # Create a vocab wrapper and add some special tokens.
-    vocab = Vocabulary()
-    vocab.add_word('[PAD]')
-    vocab.add_word('[CLS]')
-    vocab.add_word('[SEP]')
-    vocab.add_word('[UNK]')
-
-    # Add the words to the vocabulary.
-    for i, word in enumerate(words):
-        vocab.add_word(word)
-    print("Total vocabulary size: {}".format(len(vocab)))
-
-    return vocab
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--df_path', type=str, required=True,
-        help='Path for dataframe file with whole list of files and tags')
-    parser.add_argument('--vocab_path', type=str, default='./utilities/vocab.pkl')
-    parser.add_argument('--threshold', type=int, default=2, 
-        help='Minimum tag count to put into final vocabulary')
-    args = parser.parse_args()
-
-    vocab = build_vocab(args)
-    with open(args.vocab_path, 'wb') as f:
-        pickle.dump(vocab, f)
-    print('Saved vocabulary wrapper to ', args.vocab_path)
-
-if __name__ == '__main__':
-    main()
+        if self.ret_tensor:
+            return torch.tensor([tokens], dtype=torch.int64)
+        return tokens
+        
+    def decode(self, tokens_list):
+        if self.ret_tensor:
+            tokens_notensor = tokens_list.squeeze().tolist()
+            tag_list = [self.vocab.ret_word(idx) for idx in tokens_notensor]
+            return tag_list
+        else:
+            return [self.vocab.ret_word(idx) for idx in tokens_list]
 

classification/utilities/data_selection.py
@@ -1,5 +1,6 @@
 import os
 import ast
+import random
 import pandas as pd
 from PIL import Image
 from PIL import ImageFile
@@ -8,6 +9,8 @@ import torch
 import torch.utils.data as data
 from torchvision import transforms
 from transformers import BertTokenizer
+
+from .custom_tokenizer import CustomTokenizer
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -64,8 +67,10 @@ class danbooruFacesFull(data.Dataset):
 		self.image_size = args.image_size
 		self.split = split
 		self.transform = transform
-		
+
+		self.tokenizer_method = args.tokenizer		
 		self.max_text_seq_len = args.max_text_seq_len
+		self.shuffle = args.shuffle_tokens
 		
 		if self.split=='train':
 			print('Train set')
@@ -86,7 +91,12 @@ class danbooruFacesFull(data.Dataset):
 				self.transform = get_transform(split='test', image_size=self.image_size)
 
 		if self.max_text_seq_len:
-			self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+			if self.tokenizer_method == 'wp':
+				self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+			elif self.tokenizer_method == 'tag':
+				self.tokenizer = CustomTokenizer(
+					vocab_path=os.path.join(args.dataset_path, 'labels', 'vocab.pkl'), 
+                	max_text_seq_len=args.max_text_seq_len)
 			self.set_dir = self.set_dir.replace('.csv', '_tags.csv')
 			self.df = pd.read_csv(self.set_dir)
 		else:
@@ -118,9 +128,14 @@ class danbooruFacesFull(data.Dataset):
 
 		if self.max_text_seq_len:
 			caption = ast.literal_eval(self.df.iloc[idx].tags_cat0)
-			caption = ' '.join(caption) # originally joined by '[SEP]'
-			caption = self.tokenizer(caption, return_tensors='pt', padding='max_length', 
-				max_length=self.max_text_seq_len, truncation=True)['input_ids']
+			if self.shuffle:
+				random.shuffle(caption)
+			if self.tokenizer_method == 'wp':
+				caption = ' '.join(caption) # originally joined by '[SEP]'
+				caption = self.tokenizer(caption, return_tensors='pt', padding='max_length', 
+					max_length=self.max_text_seq_len, truncation=True)['input_ids']
+			elif self.tokenizer_method == 'tag':
+				caption = self.tokenizer(caption)
 			return img, target, caption
 		else:
 			return img, target

classification/utilities/scheduler.py
@@ -27,12 +27,14 @@ class WarmupCosineSchedule(LambdaLR):
 
 class MasksSchedule():
 
-    def __init__(self, device, mask_schedule, batch_size, max_text_seq_len, 
+    def __init__(self, device, mask_schedule, masking_behavior, vocab_size, batch_size, max_text_seq_len, 
         warmup_steps, cooldown_steps, total_steps, cycles=.5):
         
         self.device = device
 
         self.mask_schedule = mask_schedule
+        self.masking_behavior = masking_behavior
+        self.vocab_size = vocab_size
         
         self.batch_size = batch_size
         self.max_text_seq_len = max_text_seq_len
@@ -57,11 +59,16 @@ class MasksSchedule():
                 p=[0.15, 0.85])).to(self.device)
             
             # if mask then change token to 1 (unused 0)
-            tokens_text_updated = torch.where(((masks==0) & ((tokens_text!=0) | (tokens_text!=101) | (tokens_text!=102))), 1, tokens_text)
+            #if self.masking_behavior == 'constant':
+            #    tokens_text_updated = torch.where(((masks==0) & ((tokens_text!=0) | (tokens_text!=101) | (tokens_text!=102))), 1, tokens_text)
+            #elif self.masking_behavior == 'random':
+            #    num_to_mask = torch.where(((masks==0) & ((tokens_text!=0) | (tokens_text!=101) | (tokens_text!=102))), 1, 0).sum()
+            #    tokens_text_updated = torch.where(((masks==0) & ((tokens_text!=0) | (tokens_text!=101) | (tokens_text!=102))), 
+            #    torch.randint(0, self.vocab_size-1, (num_to_mask)), tokens_text)
             #tokens_text_updated = torch.where((masks==0) & ((tokens!=0) | (tokens!=101) | (tokens!=102)), random.randint(0, self.vocab_size-1), tokens_text)
             
             # 0 is [PAD], 101 is [CLS], 102 is [SEP]
-            labels_text = torch.where((tokens_text==0) | (tokens_text==101) | (tokens_text==102) | (masks==1), -100, tokens_text)
+            #labels_text = torch.where((tokens_text==0) | (tokens_text==101) | (tokens_text==102) | (masks==1), -100, tokens_text)
             
             ##labels_text = torch.where(masks==1, -100, labels_text)
 
@@ -70,8 +77,8 @@ class MasksSchedule():
             #labels_text = torch.where((tokens_text==0) | (tokens_text==101) | (tokens_text==102), -100, tokens_text)
             #labels_text = torch.where(masks[:, -self.max_text_seq_len:]==1, -100, labels_text)
 
-            return tokens_text_updated, labels_text
-        
+            #return tokens_text_updated, labels_text
+            
         elif self.mask_schedule == 'sigmoid':
             # during warmup attend to all tokens
             # during cooldown attend to no text tokens
@@ -100,11 +107,22 @@ class MasksSchedule():
             
             # if mask then change token to 1 (unused 0)
             #tokens_text_updated = torch.where(masks[:, -self.max_text_seq_len:]==0, 1, tokens_text)
-            tokens_text_updated = torch.where(((masks==0) & ((tokens_text!=0) | (tokens_text!=101) | (tokens_text!=102))), 1, tokens_text)
+            #tokens_text_updated = torch.where(((masks==0) & ((tokens_text!=0) | (tokens_text!=101) | (tokens_text!=102))), 1, tokens_text)
+
 
             # 0 is [PAD], 101 is [CLS], 102 is [SEP]
-            labels_text = torch.where((tokens_text==0) | (tokens_text==101) | (tokens_text==102) | (masks==1), -100, tokens_text)
             #labels_text = torch.where((tokens_text==0) | (tokens_text==101) | (tokens_text==102), -100, tokens_text)
             #labels_text = torch.where(masks[:, -self.max_text_seq_len:]==1, -100, labels_text)
-
-            return tokens_text_updated, labels_text
+        
+        if self.masking_behavior == 'constant':
+            # if mask then change token to 1 (unused 0)
+            tokens_text_updated = torch.where((masks==0) & (tokens_text!=0) & (tokens_text!=101) & (tokens_text!=102), 1, tokens_text)
+        elif self.masking_behavior == 'random':
+            random_numbers = torch.randint(0, self.vocab_size-1, (self.batch_size, self.max_text_seq_len)).to(self.device)
+            tokens_text_updated = torch.where((masks==0) & (tokens_text!=0) & (tokens_text!=101) & (tokens_text!=102), 
+                random_numbers, tokens_text)
+        
+        # 0 is [PAD], 101 is [CLS], 102 is [SEP]
+        labels_text = torch.where((tokens_text==0) | (tokens_text==101) | (tokens_text==102) | (masks==1), -100, tokens_text)
+    
+        return tokens_text_updated, labels_text
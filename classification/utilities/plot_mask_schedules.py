7e7281cf4c6f209518d185f9b223891da5c88d22
arkel23 <edwinarkelrios.eic08g@nctu.edu.tw>
2021-08-18 19:40:31 +0800

    changes in mask schedule plot

--- classification/utilities/plot_mask_schedules.py
+++ classification/utilities/plot_mask_schedules.py
@@ -5,23 +5,31 @@ import matplotlib.pyplot as plt
 
 from scheduler import MasksSchedule
 
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
+
 try:
     schedule = sys.argv[1]
 except:
     schedule = 'sigmoid'
 
 batch_size = 16
-no_epochs = 50
+no_epochs = 100
 steps_per_epoch = 20000
 total_steps = no_epochs * steps_per_epoch
 
-cdwu_percent = 0.2
+cdwu_percent = 0.1
 cdwu_steps = int(total_steps*cdwu_percent)
 
 max_text_seq_len = 16
 
+# device=device, mask_schedule=args.mask_schedule,
+#            masking_behavior=args.masking_behavior, tokenizer=args.tokenizer, vocab_size=args.vocab_size,
+#            batch_size=args.batch_size, max_text_seq_len=args.max_text_seq_len,
+#            warmup_steps=mask_wu_steps, cooldown_steps=mask_cd_steps, total_steps=total_steps, cycles=.5
+
 mask_scheduler = MasksSchedule(torch.device('cpu'), schedule,
-    batch_size, max_text_seq_len, cdwu_steps, cdwu_steps, total_steps)
+    'constant', 'wp', 30522, batch_size, max_text_seq_len, cdwu_steps, cdwu_steps, total_steps)
 
 sample_captions = torch.tensor([[101, 1015, 2611, 2630, 2159, 2829, 2606, 1015, 1015, 2611, 5967, 9427, 2849, 10557, 2159, 102]], dtype=torch.int64)
 sample_captions = sample_captions.repeat(batch_size, 1)
